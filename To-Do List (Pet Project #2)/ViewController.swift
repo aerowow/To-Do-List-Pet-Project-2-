e50f4ba77e763136ca76def385d65fa9a645f5f6
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-07-22 12:44:34 +0300

    Initial Commit

---
+++ To-Do List (Pet Project #2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List (Pet Project #2)
+//
+//  Created by Даниил Кирьянчук on 22.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
